7f09ef8c22b980c0bc235e6ce575f4632d2be38f
Martin Levesque <levesque.martin@gmail.com>
2024-08-10 21:59:54 -0400

    init commands

--- command_handler.go
+++ command_handler.go
@@ -1,12 +1,40 @@
 package main
 
 import (
+	"errors"
 	"log"
 	"regexp"
 	"strings"
 )
 
-func HandleCommand(rawCommand string) {
+type CommandType string
+
+// Define the possible values for ActionType
+const (
+	COMMAND_TERMINATE_CONN CommandType = "terminate-conn"
+	COMMAND_RETURN_KEY     CommandType = "return-key"
+)
+
+type KVOperation struct {
+	Action  CommandType
+	KeyName string
+	replyCh chan string
+}
+
+func handleQuit(_arg1 string, _arg2 string) KVOperation {
+	return KVOperation{
+		Action: COMMAND_TERMINATE_CONN,
+	}
+}
+
+func handleGet(keyName string, _arg2 string) KVOperation {
+	return KVOperation{
+		Action:  COMMAND_RETURN_KEY,
+		KeyName: keyName,
+	}
+}
+
+func HandleCommand(rawCommand string) (KVOperation, error) {
 	command := strings.TrimSpace(rawCommand)
 	log.Printf("Received command: %s", command)
 
@@ -23,13 +51,29 @@ func HandleCommand(rawCommand string) {
 	if len(matches) > 1 {
 		// First group will be the command
 		commandType := matches[1]
-		log.Printf("Command type: %s", commandType)
+		arg1 := ""
+		arg2 := ""
 
-		if len(matches) > 2 {
-			log.Printf("Command argument group 2: %s", matches[2])
+		if len(matches) > 3 {
+			arg1 = matches[3]
 		}
-	} else {
-		log.Printf("No valid command found")
+
+		if len(matches) > 5 {
+			arg2 = matches[5]
+		}
+
+		commands := map[string]func(string, string) KVOperation{
+			"QUIT": handleQuit,
+			"GET":  handleGet,
+		}
+
+		if commandFunc, found := commands[commandType]; found {
+			return commandFunc(arg1, arg2), nil
+		} else {
+			log.Printf("Unknown command type: %s", commandType)
+		}
+
 	}
 
+	return KVOperation{}, errors.New("No valid command found")
 }
--- mini_kc.go
+++ mini_kc.go
@@ -20,6 +20,19 @@ func main() {
 
 	defer server.Close()
 
+	commands_channel := make(chan KVOperation)
+
+	go func(kvOperation <-chan KVOperation) {
+		for {
+			// Wait for a command from the channel
+			currentKvOperation := <-kvOperation
+
+			log.Println("opppp Received command: ", currentKvOperation)
+
+			currentKvOperation.replyCh <- "Hello from the server!"
+		}
+	}(commands_channel)
+
 	for {
 		conn, err := server.Accept()
 
@@ -28,11 +41,11 @@ func main() {
 			continue
 		}
 
-		go handleConnection(conn)
+		go handleConnection(conn, commands_channel)
 	}
 }
 
-func handleConnection(conn net.Conn) {
+func handleConnection(conn net.Conn, commands_channel chan KVOperation) {
 	defer conn.Close()
 
 	log.Printf("Accepted connection from %s", conn.RemoteAddr())
@@ -40,15 +53,39 @@ func handleConnection(conn net.Conn) {
 	// Read the request
 	reader := bufio.NewReader(conn)
 
-	line, err := reader.ReadString('\n')
+	for {
+		line, err := reader.ReadString('\n')
 
-	if err != nil {
-		log.Fatalf("Failed to read request: %s", err)
-		return
+		if err != nil {
+			log.Fatalf("Failed to read request: %s", err)
+			return
+		}
+
+		log.Printf("Received request: %s", line)
+
+		commandResponse, err := HandleCommand(line)
+
+		if err != nil {
+			log.Printf("Failed to handle command: %s", err)
+			continue
+		}
+
+		replyCh := make(chan string)
+		commandResponse.replyCh = replyCh
+
+		if commandResponse.Action == COMMAND_TERMINATE_CONN {
+			log.Printf("Terminating connection")
+			return
+		}
+
+		commands_channel <- commandResponse
+
+		// Write the response
+		_, err = conn.Write([]byte(<-replyCh + "\n"))
+
+		if err != nil {
+			log.Printf("Failed to write response: %s", err)
+			return
+		}
 	}
-
-	log.Printf("Received request: %s", line)
-
-	HandleCommand(line)
-
 }
